b2f3e26da95ce91c11047212d135b05f33b2d628
azurerelish <azurerelish@aol.com>
2024-10-01 15:58:28 +0930

    Update extension.js
    update for gnome 46

--- gnome_shell_ethereum_gas_tracker@azurerelish.github/extension.js
+++ gnome_shell_ethereum_gas_tracker@azurerelish.github/extension.js
@@ -1,41 +1,91 @@
-const Mainloop = imports.mainloop;
-const St = imports.gi.St;
-const Main = imports.ui.main;
-const Soup = imports.gi.Soup;
+import St from 'gi://St';
+import GObject from 'gi://GObject';
+import GLib from 'gi://GLib';
+import Soup from 'gi://Soup';
+import Clutter from 'gi://Clutter';
 
-let _httpSession;
-let _indicator;
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 
-function init() {
-    _httpSession = new Soup.Session();
-    _indicator = new St.Label({ text: 'Loading...',style: 'font-size: 13px; margin: 4px;' });
-}
+const EthGasPriceIndicator = GObject.registerClass(
+    class EthGasPriceIndicator extends PanelMenu.Button {
+        _init() {
+            super._init(0.0, "Ethereum Gas Price Indicator");
 
-function enable() {
-    Main.panel._rightBox.insert_child_at_index(_indicator, 0);
-    getGasPrices();
-    Mainloop.timeout_add_seconds(15, function () {
-        getGasPrices();
-        return true; // Ensures the timeout is called again
-    });
-}
+            this.buttonText = new St.Label({
+                text: 'Loading...',
+                y_align: Clutter.ActorAlign.CENTER
+            });
+            this.add_child(this.buttonText);
 
-function disable() {
-    Main.panel._rightBox.remove_child(_indicator);
-}
-
-function getGasPrices() {
-    let message = Soup.Message.new('GET', 'https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=YourApiKeyToken');
-    _httpSession.queue_message(message, function (_httpSession, message) {
-        if (message.status_code !== 200)
-            return;
-        try {
-            let json = JSON.parse(message.response_body.data);
-            let safeGasPrice = json.result.SafeGasPrice || 'N/A';
-            let fastGasPrice = json.result.FastGasPrice || 'N/A';
-            _indicator.set_text(`Safe: ${safeGasPrice}, Fast: ${fastGasPrice}`);
-        } catch (e) {
-            _indicator.set_text('Error parsing API response');
+            this._refresh();
+            this._refreshTimeout = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 30, () => {
+                this._refresh();
+                return GLib.SOURCE_CONTINUE;
+            });
         }
-    });
+
+        _refresh() {
+            this._loadGasPrice((safePrice, fastPrice) => {
+                this.buttonText.set_text(`Safe ${safePrice} / Fast ${fastPrice}`);
+            });
+        }
+
+        _loadGasPrice(callback) {
+            let url = 'https://api.etherscan.io/api?module=gastracker&action=gasoracle';
+            let session = new Soup.Session();
+            let message = Soup.Message.new('GET', url);
+
+            session.send_and_read_async(message, GLib.PRIORITY_DEFAULT, null, (session, result) => {
+                if (message.get_status() === Soup.Status.OK) {
+                    let bytes = session.send_and_read_finish(result);
+                    let decoder = new TextDecoder('utf-8');
+                    let responseText = decoder.decode(bytes.get_data());
+                    try {
+                        let response = JSON.parse(responseText);
+                        if (response.status === "1" && response.result && response.result.SafeGasPrice && response.result.FastGasPrice) {
+                            let safePrice = parseFloat(response.result.SafeGasPrice).toFixed(2);
+                            let fastPrice = parseFloat(response.result.FastGasPrice).toFixed(2);
+                            callback(safePrice, fastPrice);
+                        } else {
+                            log(`Unexpected API response: ${responseText}`);
+                            callback('API Error', 'API Error');
+                        }
+                    } catch (e) {
+                        log(`Error parsing API response: ${e.message}`);
+                        log(`Response text: ${responseText}`);
+                        callback('Parse Error', 'Parse Error');
+                    }
+                } else {
+                    log(`HTTP Error: ${message.get_status()}`);
+                    callback('HTTP Error', 'HTTP Error');
+                }
+            });
+        }
+
+        destroy() {
+            if (this._refreshTimeout) {
+                GLib.source_remove(this._refreshTimeout);
+            }
+            super.destroy();
+        }
+    }
+);
+
+export default class Extension {
+    constructor() {
+        this._indicator = null;
+    }
+
+    enable() {
+        this._indicator = new EthGasPriceIndicator();
+        Main.panel.addToStatusArea('eth-gas-price', this._indicator);
+    }
+
+    disable() {
+        if (this._indicator) {
+            this._indicator.destroy();
+            this._indicator = null;
+        }
+    }
 }
